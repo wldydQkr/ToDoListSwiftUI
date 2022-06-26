c12d1a222f6343db1c54c821159e2513834e0be7
wlQkr <wlqkr1010@gmail.com>
2022-06-26 18:14:06 +0900

    TabView 추가

---
+++ ToDoListSwiftUI/MainTabView.swift
@@ -0,0 +1,31 @@
+//
+//  TabView.swift
+//  ToDoListSwiftUI
+//
+//  Created by 박지용 on 2022/06/26.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            ContentView()
+                .tabItem {
+                    Image(systemName: "note.text")
+                    Text("First")
+                }
+            Text("Other")
+                .tabItem {
+                    Image(systemName: "person")
+                    Text("Second")
+                }
+        }
+    }
+}
+
+struct MainTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTabView()
+    }
+}
